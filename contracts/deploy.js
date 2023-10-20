e66af4da70d4dbc178d760793711d662704a369a
kimjaeyeol <kjy@opusm.io>
2023-10-20 15:50:22 +0900

    gasLimit

--- contracts/deploy.js
+++ contracts/deploy.js
@@ -63,7 +63,8 @@ async function createContract(
     contractByteCode,
     wallet
   );
-  const contract = await factory.deploy({ gasLimit: 1e8480 });
+  const gasLimit = ethers.BigNumber.from("1e18");
+  const contract = await factory.deploy({ gasLimit: gasLimit });
   // The contract is NOT deployed yet; we must wait until it is mined
   const deployed = await contract.waitForDeployment();
   //The contract is deployed now
