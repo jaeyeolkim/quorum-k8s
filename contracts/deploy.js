3c63103545ebaf943b716db94aa594625b3cd3a0
kimjaeyeol <kjy@opusm.io>
2023-10-20 15:41:17 +0900

    TypeError: invalid BigNumberish value

--- contracts/deploy.js
+++ contracts/deploy.js
@@ -63,7 +63,7 @@ async function createContract(
     contractByteCode,
     wallet
   );
-  const contract = await factory.deploy({ gasLimit: 2000000 });
+  const contract = await factory.deploy({ gasLimit: 0x2000000 });
   // The contract is NOT deployed yet; we must wait until it is mined
   const deployed = await contract.waitForDeployment();
   //The contract is deployed now
