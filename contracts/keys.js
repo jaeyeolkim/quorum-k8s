26f29f64ab5e575c350edf015a69b5a784bb2dd3
kimjaeyeol <kjy@opusm.io>
2023-10-20 13:48:14 +0900

    Revert accountPrivateKey

--- contracts/keys.js
+++ contracts/keys.js
@@ -4,7 +4,7 @@ module.exports = {
       url: "http://10.233.103.129:8545",
       accountAddress: "0x11d0dc20bcead1e3cfef60e5b0af2d99d9534d0e",
       accountPrivateKey:
-        "0xb2fb4b0a56595f6e3623ff1e46c218e0194786156ea241bd073f532e3fae8cb9",
+        "0x69cd36c00f8413a26a5b42ded6be63fd0630c871433cd34751917cbdf6cc3344",
     },
   },
 };
