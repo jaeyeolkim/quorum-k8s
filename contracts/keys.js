e4c37c423e6519c737d86fc0236f2b3149d5b5cd
kimjaeyeol <kjy@opusm.io>
2023-10-25 11:38:16 +0900

    Add validator

--- contracts/keys.js
+++ contracts/keys.js
@@ -6,5 +6,10 @@ module.exports = {
       accountPrivateKey:
         "0xa132ad30748bd38aa0f4b1229b6fc4c6544bde42cc9c5034eb4b764f2ae0340e",
     },
+    validator: {
+      url: "http://10.233.62.225:8545",
+      accountPrivateKey:
+        "0x8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63",
+    },
   },
 };
