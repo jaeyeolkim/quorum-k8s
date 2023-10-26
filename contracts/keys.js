156e1ea0e10a0ab49cdd4971cb90796ae5875afb
kimjaeyeol <kjy@opusm.io>
2023-10-26 22:38:43 +0900

    key

--- contracts/keys.js
+++ contracts/keys.js
@@ -7,7 +7,7 @@ module.exports = {
         "0xa132ad30748bd38aa0f4b1229b6fc4c6544bde42cc9c5034eb4b764f2ae0340e",
     },
     validator: {
-      url: "http://10.233.34.19:8545",
+      url: "http://10.233.16.119:8545",
       accountPrivateKey:
         "0x8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63",
     },
