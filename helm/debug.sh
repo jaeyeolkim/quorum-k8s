2b2dcdfea4e73e352630e668c0c0e40ed432f6a0
kimjaeyeol <kjy@opusm.io>
2023-11-07 10:06:21 +0900

    Add genesis --debug

--- helm/debug.sh
+++ helm/debug.sh
@@ -1,2 +1,4 @@
-helm install validator-1 ./charts/goquorum-node --namespace quorum --values ./values/validator.yml --dry-run --debug > debug.yaml
-# scp -i ~/.ssh/lightsail_ubuntu1.pem ubuntu@13.125.35.215:/home/ubuntu/quorum-k8s/helm/debug.yaml workspace/opusm/quorum-k8s/helm/debug
+# scp -i ~/.ssh/lightsail_ubuntu1.pem ubuntu@13.125.35.215:/home/ubuntu/quorum-k8s/helm/genesis.yaml workspace/opusm/quorum-k8s/helm/debug
+# scp -i ~/.ssh/lightsail_ubuntu1.pem ubuntu@13.125.35.215:/home/ubuntu/quorum-k8s/helm/validator1.yaml workspace/opusm/quorum-k8s/helm/debug
+helm install genesis ./charts/goquorum-genesis --namespace quorum --create-namespace --values ./values/genesis-goquorum.yml --dry-run --debug > genesis.yaml
+helm install validator-1 ./charts/goquorum-node --namespace quorum --values ./values/validator.yml --dry-run --debug > validator1.yaml
