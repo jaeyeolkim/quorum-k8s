4903de7796cfb18e0e78969337b52eb427438a16
kimjaeyeol <kjy@opusm.io>
2023-11-07 11:10:24 +0900

    validator 5 개로

--- helm/deploy.sh
+++ helm/deploy.sh
@@ -1,2 +1,6 @@
 helm install genesis ./charts/goquorum-genesis --namespace quorum --create-namespace --values ./values/genesis-goquorum.yml
 helm install validator-1 ./charts/goquorum-node --namespace quorum --values ./values/validator.yml
+helm install validator-2 ./charts/goquorum-node --namespace quorum --values ./values/validator.yml
+helm install validator-3 ./charts/goquorum-node --namespace quorum --values ./values/validator.yml
+helm install validator-4 ./charts/goquorum-node --namespace quorum --values ./values/validator.yml
+helm install validator-5 ./charts/goquorum-node --namespace quorum --values ./values/validator.yml
