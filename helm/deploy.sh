983b7bb88ab9567b19c8f724b49f0ac027621f97
kimjaeyeol <kjy@opusm.io>
2023-11-07 16:22:56 +0900

    error: the path "rbac.yml" does not exist

--- helm/deploy.sh
+++ helm/deploy.sh
@@ -1,4 +1,7 @@
-../nfs/deploy.sh
+k apply -f ../nfs/rbac.yml
+k apply -f ../nfs/deployment.yml
+k apply -f ../nfs/storage.yml
+k apply -f ../nfs/pvc.yml
 helm install genesis ./charts/goquorum-genesis --namespace quorum --create-namespace --values ./values/genesis-goquorum.yml
 helm install validator-1 ./charts/goquorum-node --namespace quorum --values ./values/validator.yml
 helm install validator-2 ./charts/goquorum-node --namespace quorum --values ./values/validator.yml
