5696b3ac16a8770ee1f5a287cf10210f0358ad50
kimjaeyeol <kjy@opusm.io>
2023-11-07 13:35:27 +0900

    validator5 -> rpc

--- helm/deploy.sh
+++ helm/deploy.sh
@@ -3,4 +3,4 @@ helm install validator-1 ./charts/goquorum-node --namespace quorum --values ./va
 helm install validator-2 ./charts/goquorum-node --namespace quorum --values ./values/validator.yml
 helm install validator-3 ./charts/goquorum-node --namespace quorum --values ./values/validator.yml
 helm install validator-4 ./charts/goquorum-node --namespace quorum --values ./values/validator.yml
-helm install validator-5 ./charts/goquorum-node --namespace quorum --values ./values/validator.yml
+helm install rpc-1 ./charts/goquorum-node --namespace quorum --values ./values/reader.yml
