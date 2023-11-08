ff4b542b9a07ed38d7deb7401c48c059e581b500
kimjaeyeol <kjy@opusm.io>
2023-11-08 14:01:19 +0900

    curl test

--- helm/curl.sh
+++ helm/curl.sh
@@ -1,3 +1,3 @@
-index=$1
+index=$1|1
 k get pod goquorum-node-validator-${index}-0 --template '{{.status.podIP}}'
-curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":1}' http://$1:8545
+curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":1}' http://$?:8545
