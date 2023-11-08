b7b29faa2387202714fc4d0c673179d602fae397
kimjaeyeol <kjy@opusm.io>
2023-11-08 14:15:46 +0900

    eth_blockNumber

--- helm/curl.sh
+++ helm/curl.sh
@@ -4,8 +4,8 @@ index=$1
 
 function net_peerCount {
   clusterIP=$1
-  echo ${clusterIP}
   curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":1}' http://${clusterIP}:8545
+  curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' http://${clusterIP}:8545
 }
 
 net_peerCount $(k get svc goquorum-node-validator-${index} -o jsonpath='{.spec.clusterIP}')
