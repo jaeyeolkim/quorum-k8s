803fb16575e84acd088d8962c8a8cf7e44fc05b5
kimjaeyeol <kjy@opusm.io>
2023-11-08 14:22:17 +0900

    curl 1-5

--- helm/curl.sh
+++ helm/curl.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-index=$1
-
-function net_peerCount {
+function geth_attach {
   clusterIP=$1
   curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":1}' http://${clusterIP}:8545
   curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' http://${clusterIP}:8545
 }
 
-net_peerCount $(k get svc goquorum-node-validator-${index} -o jsonpath='{.spec.clusterIP}')
+
+for i in {1..5}
+do
+    echo "validator-$i"
+    geth_attach $(k get svc goquorum-node-validator-${i} -o jsonpath='{.spec.clusterIP}')
+done
