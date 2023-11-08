ae11de33abe3e22bb8214c4d44d606b1ce474531
kimjaeyeol <kjy@opusm.io>
2023-11-08 14:10:35 +0900

    clusterIP

--- helm/curl.sh
+++ helm/curl.sh
@@ -1,3 +1,11 @@
-index=$1|1
-k get pod goquorum-node-validator-${index}-0 --template '{{.status.podIP}}'
-curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":1}' http://$?:8545
+#!/bin/bash
+
+index=$1
+
+function net_peerCount {
+  k get svc goquorum-node-validator-${index} -o jsonpath='{.spec.clusterIP}'
+  clusterIP=$?
+  curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":1}' http://${clusterIP}:8545
+}
+
+net_peerCount
