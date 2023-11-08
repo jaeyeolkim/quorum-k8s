b139861a144c71370fa49c1d51a3551909d2b5c5
kimjaeyeol <kjy@opusm.io>
2023-11-08 14:14:28 +0900

    오타

--- helm/curl.sh
+++ helm/curl.sh
@@ -8,4 +8,4 @@ function net_peerCount {
   curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":1}' http://${clusterIP}:8545
 }
 
-net_peerCount ${k get svc goquorum-node-validator-${index} -o jsonpath='{.spec.clusterIP}')
+net_peerCount $(k get svc goquorum-node-validator-${index} -o jsonpath='{.spec.clusterIP}')
