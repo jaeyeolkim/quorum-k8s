d66c743ee0cd70caf55cd3d9aa6b2c3153fd662b
kimjaeyeol <kjy@opusm.io>
2023-11-08 16:12:13 +0900

    Remove hook

--- helm/curl.sh
+++ helm/curl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function geth_attach {
+function geth_method {
   clusterIP=$1
   curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":1}' http://${clusterIP}:8545
   curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' http://${clusterIP}:8545
@@ -10,5 +10,5 @@ function geth_attach {
 for i in {1..5}
 do
     echo "======== validator-$i ========"
-    geth_attach $(k get svc goquorum-node-validator-${i} -o jsonpath='{.spec.clusterIP}')
+    geth_method $(k get svc goquorum-node-validator-${i} -o jsonpath='{.spec.clusterIP}')
 done
