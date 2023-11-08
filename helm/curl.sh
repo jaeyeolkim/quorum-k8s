81859533105b5f586eabd91ebc386a868950e483
kimjaeyeol <kjy@opusm.io>
2023-11-08 14:13:54 +0900

    함수 호출 방식 변경

--- helm/curl.sh
+++ helm/curl.sh
@@ -3,10 +3,9 @@
 index=$1
 
 function net_peerCount {
-  k get svc goquorum-node-validator-${index} -o jsonpath='{.spec.clusterIP}'
-  clusterIP=$?
+  clusterIP=$1
   echo ${clusterIP}
   curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":1}' http://${clusterIP}:8545
 }
 
-net_peerCount
+net_peerCount ${k get svc goquorum-node-validator-${index} -o jsonpath='{.spec.clusterIP}')
