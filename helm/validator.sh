d11320948b9aa50e803aec2b67e88a4979af9c18
kimjaeyeol <kjy@opusm.io>
2023-11-08 16:28:32 +0900

    validator 3

--- helm/validator.sh
+++ helm/validator.sh
@@ -3,7 +3,7 @@ sleep 3
 helm install validator-2 ./charts/goquorum-node --namespace quorum --values ./values/validator.yml
 sleep 3
 helm install validator-3 ./charts/goquorum-node --namespace quorum --values ./values/validator.yml
-sleep 3
-helm install validator-4 ./charts/goquorum-node --namespace quorum --values ./values/validator.yml
-sleep 3
-helm install validator-5 ./charts/goquorum-node --namespace quorum --values ./values/validator.yml
+# sleep 3
+# helm install validator-4 ./charts/goquorum-node --namespace quorum --values ./values/validator.yml
+# sleep 3
+# helm install validator-5 ./charts/goquorum-node --namespace quorum --values ./values/validator.yml
