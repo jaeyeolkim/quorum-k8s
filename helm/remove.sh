6a6b46a3819eae91c1b3764565fb7581cebbd809
kimjaeyeol <kjy@opusm.io>
2023-11-09 15:51:20 +0900

    clusterroles

--- helm/remove.sh
+++ helm/remove.sh
@@ -1,3 +1,3 @@
 helm uninstall validator-1 validator-2 validator-3 validator-4 validator-5 genesis
-k delete rs,deploy,po,job,cm,pv,pvc,secret --all
+k delete rs,deploy,po,job,cm,pv,pvc,secret,clusterrolebindings,clusterroles --all
 k delete ns quorum
