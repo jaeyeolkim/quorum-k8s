8c6220a8db89d1ca405f3ef8db4c90c24942321c
kimjaeyeol <kjy@opusm.io>
2023-11-09 15:57:53 +0900

    delete

--- helm/remove.sh
+++ helm/remove.sh
@@ -1,3 +1,8 @@
 helm uninstall validator-1 validator-2 validator-3 validator-4 validator-5 genesis
-k delete rs,deploy,po,job,cm,pv,pvc,secret,clusterrolebindings,clusterroles --all
+k delete rs,deploy,po,job,cm,pv,pvc,secret --all
+k delete serviceaccount/nfs-client-provisioner
+k delete clusterrole.rbac.authorization.k8s.io/nfs-client-provisioner-runner
+k delete clusterrolebinding.rbac.authorization.k8s.io/run-nfs-client-provisioner
+k delete role.rbac.authorization.k8s.io/leader-locking-nfs-client-provisioner
+k delete rolebinding.rbac.authorization.k8s.io/leader-locking-nfs-client-provisioner
 k delete ns quorum
