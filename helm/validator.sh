554fcd396d71188bb0989d57e5c64f7cebf4bc0d
kimjaeyeol <kjy@opusm.io>
2023-11-09 14:43:47 +0900

    Test peercount

--- helm/validator.sh
+++ helm/validator.sh
@@ -1,9 +1,9 @@
 helm install validator-1 ./charts/goquorum-node --namespace quorum --values ./values/validator.yml
-sleep 3
+sleep 60
 helm install validator-2 ./charts/goquorum-node --namespace quorum --values ./values/validator.yml
-sleep 3
+sleep 60
 helm install validator-3 ./charts/goquorum-node --namespace quorum --values ./values/validator.yml
-sleep 3
+sleep 60
 helm install validator-4 ./charts/goquorum-node --namespace quorum --values ./values/validator.yml
 # sleep 3
 # helm install validator-5 ./charts/goquorum-node --namespace quorum --values ./values/validator.yml
