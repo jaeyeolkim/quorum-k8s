b9c0107aab05b3692699c48e30203410c24e3e42
kimjaeyeol <kjy@opusm.io>
2023-11-14 11:09:59 +0900

    cannot unmarshal hex string without 0x

--- qbft/curl.sh
+++ qbft/curl.sh
@@ -6,7 +6,7 @@ rpcNodeAddress="$(echo $rpcNode | grep -Po '(?<="result":")[^"]+')"
 
 function geth_method {
   clusterIP=$1
-  curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"istanbul_propose","params":["$rpcNodeAddress",false],"id":1}' http://${clusterIP}:8545
+  curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"istanbul_propose","params":[""$rpcNodeAddress"",false],"id":1}' http://${clusterIP}:8545
   curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"istanbul_isValidator","params":[],"id":1}' http://${clusterIP}:8545
   curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":1}' http://${clusterIP}:8545
   curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' http://${clusterIP}:8545
