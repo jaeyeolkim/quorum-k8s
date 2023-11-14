17c2775a080f18024e50cc64419b08c91a5a1761
kimjaeyeol <kjy@opusm.io>
2023-11-14 13:59:29 +0900

    istanbul_nodeAddress

--- qbft/curl.sh
+++ qbft/curl.sh
@@ -7,8 +7,8 @@ rpcNodeAddress=\"$(echo $rpcNode | grep -Po '(?<="result":")[^"]+')\"
 function geth_method {
   clusterIP=$1
   curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"istanbul_propose","params":['${rpcNodeAddress}',false],"id":1}' http://${clusterIP}:8545
-  curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"istanbul_getValidators","params":[],"id":1}' http://${clusterIP}:8545
   curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"istanbul_isValidator","params":[],"id":1}' http://${clusterIP}:8545
+  curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"istanbul_nodeAddress","params":[],"id":1}' http://${clusterIP}:8545
   curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":1}' http://${clusterIP}:8545
   curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' http://${clusterIP}:8545
 }
@@ -18,3 +18,5 @@ do
     echo "======== validator-$i ========"
     geth_method $(k get svc quorum-validator${i} -o jsonpath='{.spec.clusterIP}')
 done
+
+curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"istanbul_getValidators","params":[],"id":1}' http://${clusterIP}:8545
