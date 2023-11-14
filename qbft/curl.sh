7d77b340f96bd438bd27927c71ef0c9e39997a03
kimjaeyeol <kjy@opusm.io>
2023-11-14 10:29:21 +0900

    istanbul_isValidator

--- qbft/curl.sh
+++ qbft/curl.sh
@@ -2,6 +2,8 @@
 
 function geth_method {
   clusterIP=$1
+  curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"istanbul_isValidator","params":[],"id":1}' http://${clusterIP}:8545
+  curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"istanbul_nodeAddress","params":[],"id":1}' http://${clusterIP}:8545
   curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":1}' http://${clusterIP}:8545
   curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' http://${clusterIP}:8545
 }
