f8711d344157998fb2c102ffda2093d386778a00
kimjaeyeol <kjy@opusm.io>
2023-11-15 16:19:45 +0900

    rpc -> validator

--- opusm/kubectl/curl.sh
+++ opusm/kubectl/curl.sh
@@ -8,15 +8,24 @@ function geth_method {
   curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' http://${clusterIP}:8545
 }
 
-for i in {0..4}
+for i in {1..5}
 do
-  if [[ i -eq 0 ]]; then
-      SVC_NAME="goquorum-node-rpc-${i+1}"
-  else
-      SVC_NAME="goquorum-node-validator-${i}"
-  fi
+    SVC_NAME="goquorum-node-validator-${i}"
     echo "======== $SVC_NAME ========"
     geth_method $(k get svc $SVC_NAME -o jsonpath='{.spec.clusterIP}')
 done
 
 curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"istanbul_getValidators","params":[],"id":1}' http://${clusterIP}:8545
+
+# for i in {0..4}
+# do
+#   if [[ i -eq 0 ]]; then
+#       SVC_NAME="goquorum-node-rpc-${i+1}"
+#   else
+#       SVC_NAME="goquorum-node-validator-${i}"
+#   fi
+#     echo "======== $SVC_NAME ========"
+#     geth_method $(k get svc $SVC_NAME -o jsonpath='{.spec.clusterIP}')
+# done
+
+# curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"istanbul_getValidators","params":[],"id":1}' http://${clusterIP}:8545
