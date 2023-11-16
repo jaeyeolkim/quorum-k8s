d4b235efcd87dab9bcf2de187aa593bde15b544e
kimjaeyeol <kjy@opusm.io>
2023-11-16 11:30:09 +0900

    install.sh 로 통합

--- opusm/install_node.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in {1..5}; do
-  RELEASE_NAME="validator-${i}"
-  helm install $RELEASE_NAME ./helm/charts/goquorum-node --namespace quorum --values ./helm/values/validator.yaml
-  sleep 5
-done
--- opusm/run.sh
+++ opusm/run.sh
@@ -31,7 +31,7 @@ for i in {1..5}; do
 
 done
 
-echo "✏️ goquorum-node-validator-1 -> goquorum-node-rpc-1"
+echo "✏️  goquorum-node-validator-1 -> goquorum-node-rpc-1"
 rpcNodeIP=$(k get svc goquorum-node-validator-1 -o jsonpath='{.spec.clusterIP}')
 rpcNode=$(curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"istanbul_nodeAddress","params":[],"id":1}' http://${rpcNodeIP}:8545)
 rpcNodeAddress=\"$(echo $rpcNode | grep -Po '(?<="result":")[^"]+')\"
