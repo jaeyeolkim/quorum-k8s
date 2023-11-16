641f976e831e813e7ed90bc0130bb8404fce98b3
kimjaeyeol <kjy@opusm.io>
2023-11-16 10:10:06 +0900

    rpc 분리

--- opusm/run.sh
+++ opusm/run.sh
@@ -58,24 +58,3 @@ for i in {1..5}; do
     echo "----------------------------------------------------------------"
 
 done
-
-
-rpcNodeIP=$(k get svc goquorum-node-validator-5 -o jsonpath='{.spec.clusterIP}')
-rpcNode=$(curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"istanbul_nodeAddress","params":[],"id":1}' http://${rpcNodeIP}:8545)
-rpcNodeAddress=\"$(echo $rpcNode | grep -Po '(?<="result":")[^"]+')\"
-echo "rpcNodeAddress: $rpcNodeAddress"
-
-function geth_method {
-  clusterIP=$1
-  curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"istanbul_propose","params":['${rpcNodeAddress}',false],"id":1}' http://${clusterIP}:8545
-}
-
-
-for j in {1..5}
-do
-    SVC_NAME="goquorum-node-validator-${j}"
-    echo "======== $SVC_NAME ========"
-    geth_method $(k get svc $SVC_NAME -o jsonpath='{.spec.clusterIP}')
-done
-
-curl -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"istanbul_getValidators","params":[],"id":1}' http://${clusterIP}:8545
