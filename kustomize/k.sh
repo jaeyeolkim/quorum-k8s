c223d34272e06055c4ce99be360f4cfb2d1056b1
kimjaeyeol <kjy@opusm.io>
2023-11-27 15:26:25 +0900

    test

--- kustomize/k.sh
+++ kustomize/k.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# genesis quorum
-npx quorum-genesis-tool --consensus qbft --validators 5 --chainID 1337 --blockperiod 1 --requestTimeout 10 --epochLength 30000 --difficulty 1 --gasLimit '0xFFFFFF' --coinbase '0x0000000000000000000000000000000000000000' --members 0 --bootnodes 0 --outputPath 'artifacts'
-
-yyyy=$(date '+%Y')
-mv artifacts/$yyyy-*/* artifacts
-rm -rf artifacts/$yyyy-*
-
-# build
-mkdir -p ./build/statefulsets
-kustomize build > ./build/statefulsets/validator1-statefulset.yaml
-
-
 cat <<EOF > volumes.yaml
 apiVersion: apps/v1
 kind: StatefulSet
