102f5758e1f9d03e1265e85d8d22eea46e7bb11a
kimjaeyeol <kjy@opusm.io>
2023-11-29 14:10:23 +0900

    POD_NAME

--- genesis/jsonrpc.sh
+++ genesis/jsonrpc.sh
@@ -2,9 +2,8 @@
 
 for i in {1..5}
 do
-  SVC_NAME="quorum-validator${i}"
-  echo "======== $SVC_NAME ========"
-  POD_NAME=$1
+  POD_NAME="validator${i}-0"
+  echo "======== $POD_NAME ========"
   kubectl exec $POD_NAME -- curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":1}' http://localhost:8545
   kubectl exec $POD_NAME -- curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' http://localhost:8545
 done
