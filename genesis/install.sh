0578bb22b5a1145f2e4922065c876e059c49cc18
kimjaeyeol <kjy@opusm.io>
2023-11-30 11:16:03 +0900

    문자열 비교

--- genesis/install.sh
+++ genesis/install.sh
@@ -8,15 +8,15 @@ echo -n "1. Consensus? [1.qbft(default), 2.ibft]"
 read -r QUORUM_CONSENSUS
 QUORUM_CONSENSUS="${QUORUM_CONSENSUS:=qbft}"
 
-echo -n "2. Validators? [4, 5(default), 6, 7]"
+echo -n "2. Validators? [4, 5(default), 6, 7]: "
 read -r QUORUM_VALIDATORS
 QUORUM_VALIDATORS="${QUORUM_VALIDATORS:=4}"
 
-echo -n "3. ChainID? [1337(default), ...]"
+echo -n "3. ChainID? [1337(default), ...]: "
 read -r QUORUM_NETWORK_ID
 QUORUM_NETWORK_ID="${QUORUM_NETWORK_ID:=1337}"
 
-echo -n "4. Blockperiod? [1(default), ...]"
+echo -n "4. Blockperiod? [1(default), ...]: "
 read -r QUORUM_BLOCKPERIOD
 QUORUM_BLOCKPERIOD="${QUORUM_BLOCKPERIOD:=1}"
 
@@ -25,11 +25,11 @@ echo "--consensus: $QUORUM_CONSENSUS"
 echo "--validators: $QUORUM_VALIDATORS"
 echo "--chainID: $QUORUM_NETWORK_ID"
 echo "--blockperiod: $QUORUM_BLOCKPERIOD"
-echo -n "Are you sure you want to create a quorum? [Y/n]"
+echo -n "Are you sure you want to create a quorum? [Y/n]: "
 read -r quorum_create_confirm
-quorum_create_confirm="${quorum_create_confirm:=Y}"
+quorum_create_confirm="${quorum_create_confirm:="Y"}"
 
-if [[ $quorum_create_confirm -ne Y ]]; then
+if [[ "$quorum_create_confirm" != "Y" ]]; then
     exit 1
 fi
 echo "🚀 Genesis Quorum..."
