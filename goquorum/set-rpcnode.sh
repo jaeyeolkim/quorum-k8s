3a1a4872da65c48ef1aa655a9631305c0e1b517e
kimjaeyeol <kjy@opusm.io>
2023-12-06 16:42:18 +0900

    bugfix

--- goquorum/set-rpcnode.sh
+++ goquorum/set-rpcnode.sh
@@ -10,13 +10,8 @@ do
   echo "======== $POD_NAME ========"
 
   if [ $QUORUM_RPC_NODE -gt 1 ] && [ $i -lt 3 ];then
-    if [ $i -eq 1 ];then
-      rpcNode=$(kubectl exec $POD_NAME -- curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"istanbul_nodeAddress","params":[],"id":1}' http://localhost:8545)
-      rpcNodeAddress=\"$(echo $rpcNode | grep -Po '(?<="result":")[^"]+')\"
-    elif [ $i -eq 2 ];then
-      rpcNode=$(kubectl exec -c validator1 $POD_NAME -- curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"istanbul_nodeAddress","params":[],"id":1}' http://localhost:8545)
-      rpcNodeAddress=\"$(echo $rpcNode | grep -Po '(?<="result":")[^"]+')\"
-    fi
+    rpcNode=$(kubectl exec -c validator$i $POD_NAME -- curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"istanbul_nodeAddress","params":[],"id":1}' http://localhost:8545)
+    rpcNodeAddress=\"$(echo $rpcNode | grep -Po '(?<="result":")[^"]+')\"
   fi
 
   kubectl exec -c validator$i $POD_NAME -- curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"istanbul_propose","params":['${rpcNodeAddress}',false],"id":1}' http://localhost:8545
