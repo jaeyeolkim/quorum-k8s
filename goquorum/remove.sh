e24014694e37f766bba6c44f648de9d8ce136278
kimjaeyeol <kjy@opusm.io>
2023-12-06 17:08:13 +0900

    kubectl delete pv,pvc --all -n quorum

--- goquorum/remove.sh
+++ goquorum/remove.sh
@@ -4,5 +4,6 @@ kubectl delete -f secrets/
 kubectl delete configmap goquorum-genesis-configmap -n quorum
 kubectl delete configmap quorum-permissions-nodes-configmap -n quorum
 kubectl delete configmap quorum-static-nodes-configmap -n quorum
+kubectl delete pv,pvc --all -n quorum
 kubectl delete -f services/
 kubectl delete -f namespace/
